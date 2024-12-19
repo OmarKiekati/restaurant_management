ce7b7548d82a7e2a2408e066beecf25b7c006f5c
ammar <ammarsafi2005@gmail.com>
2024-12-19 14:41:55 +0300

    users.db

---
+++ users.sql
@@ -0,0 +1,8 @@
+﻿create table users (
+userID int primary key identity,
+username varchar(50) not null,
+uPass varchar(10) not null,
+uName varchar(50) not null,
+uPhone varchar(20),
+)
+insert into users values('admin',123,'User 1', '0988609771')
